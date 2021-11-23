27c820fd5602c435357688c07007b2019faceeff
g-parki <61096711+g-parki@users.noreply.github.com>
2021-11-22 22:32:52 -0800

    Updates

---
+++ precache-manifest.bd0cbacce064ca3284ab79524c0cd2b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d66da72fc56782a45a08c0f06816262b",
+    "url": "/PopItNow-React/index.html"
+  },
+  {
+    "revision": "eb38d4c986f0e41806e1",
+    "url": "/PopItNow-React/static/css/main.fd316a6d.chunk.css"
+  },
+  {
+    "revision": "d675dc1368560807ae79",
+    "url": "/PopItNow-React/static/js/2.f8af3742.chunk.js"
+  },
+  {
+    "revision": "3d8ab221ab5ca341acdd72ea533d43b7",
+    "url": "/PopItNow-React/static/js/2.f8af3742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb38d4c986f0e41806e1",
+    "url": "/PopItNow-React/static/js/main.ccf206ef.chunk.js"
+  },
+  {
+    "revision": "5eee4fce989120217b10",
+    "url": "/PopItNow-React/static/js/runtime-main.35c5bfff.js"
+  }
+]);
