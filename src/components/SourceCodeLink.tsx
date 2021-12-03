c2f351364b4d51d46a6ef22dcdd1077e0fba8c61
g-parki <61096711+g-parki@users.noreply.github.com>
2021-12-03 14:19:11 -0800

    Meaningful alt text for github logo

--- src/components/SourceCodeLink.tsx
+++ src/components/SourceCodeLink.tsx
@@ -7,7 +7,7 @@ interface SourceCodeProps {
 const SourceCodeLink: React.FunctionComponent<SourceCodeProps> = () => {
     const GITHUB_LINK = "https://github.com/g-parki/PopItNow-React"
     const LOGO_LINK = process.env.PUBLIC_URL + "/GitHub-Mark-120px-plus.png"
-    return (<a href={GITHUB_LINK}><img className="logo" src={LOGO_LINK} alt={"Didn't load"}/></a>);
+    return (<a href={GITHUB_LINK}><img className="logo" src={LOGO_LINK} alt={"GitHub logo"}/></a>);
 }
  
 export default SourceCodeLink;
